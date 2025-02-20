669122c52f8a7d63bb4f6bb20b2516f8445cfffb
Roland <roland.krancz@gmail.com>
2025-02-20 19:44:32 +0100

    Brainstorming with Blanka

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('visible');
-        } else {
-            entry.target.classList.remove('visible');
         }
     });
 });
